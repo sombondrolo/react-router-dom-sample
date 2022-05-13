8f85f61bc53b039f641aa5d0b5a9e0197a04e6a8
Fer <sombondrolo@gmail.com>
2022-05-13 16:48:45 -0300

    react-router sample

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
-function App() {
-  return (
-    <div className="App">
-      React App
-    </div>
-  );
-}
+import { Routes, Route } from "react-router-dom";
+import { Home, About, Services, Events, Products, Contact, Error404 } from "./pages";
 
-export default App;
+export function App() {
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="about" element={<About />}>
+          <Route path="services" element={<Services /> } />
+        </Route>
+        <Route path="/events" element={<Events />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="*" element={<Error404 />} />
+      </Routes>
+    </div>
+  )
+}
